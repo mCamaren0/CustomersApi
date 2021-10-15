42d84ac90caf45a5cd9b27b8c3397d2b767be8d3
facam <facam@DESKTOP-S31I4A0>
2021-10-14 21:18:47 -0600

    sesion-03 terminada

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://ip172-18-0-44-c5kdbe7njsv000civu80-5001.direct.labs.play-with-docker.com");
+            var json = await httpClient.GetStringAsync("/api/products");
+
+            Console.WriteLine(json);
+            Console.WriteLine("==========> hello world");
+        }
+    }
+}
--- CustomersApi/Program.cs
+++ CustomersApi/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using System;
 
 WebHost.CreateDefaultBuilder().Configure(app =>
 {
@@ -11,5 +12,6 @@ WebHost.CreateDefaultBuilder().Configure(app =>
     {
         e.MapGet("/", c => c.Response.WriteAsync("Hello world!"));
         e.MapGet("hello/{name}", c => c.Response.WriteAsync($"Hello, {c.Request.RouteValues["name"]}"));
+        e.MapGet("api/v1/customers", c => c.Response.WriteAsync($"Hello, {Array.Empty<object>()}"));
     });
 }).Build().Run();
